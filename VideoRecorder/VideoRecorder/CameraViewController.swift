bc1d309302b66015f28e9e3a4773ecec6013c52d
Jonalynn Masters <jonalynna.masters@gmail.com>
2020-01-15 09:57:43 -0800

    implemented code so camera works

--- VideoRecorder/VideoRecorder/CameraViewController.swift
+++ VideoRecorder/VideoRecorder/CameraViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
 
+    lazy private var captureSession = AVCaptureSession()
+    
     @IBOutlet var recordButton: UIButton!
     @IBOutlet var cameraView: CameraPreviewView!
 
@@ -19,13 +22,60 @@ class CameraViewController: UIViewController {
 
 		// Resize camera preview to fill the entire screen
 		cameraView.videoPlayerView.videoGravity = .resizeAspectFill
+        
+        setUpCamera()
 	}
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        captureSession.startRunning()
+    }
 
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        captureSession.stopRunning()
+    }
+    
     @IBAction func recordButtonPressed(_ sender: Any) {
 
 	}
+    
+    // MARK: Methods
+    
+    func setUpCamera() {
+        let camera = bestCamera()
+        captureSession.beginConfiguration()
+        // make changes to the devices connected
+        // Video input
+        guard let cameraInput = try? AVCaptureDeviceInput(device: camera) else {
+            fatalError("Cannot create camera input")
+        }
+        guard captureSession.canAddInput(cameraInput) else {
+            fatalError("Cannot add camera input to session")
+        }
+        captureSession.addInput(cameraInput)
+        if captureSession.canSetSessionPreset(.hd1920x1080) {
+            captureSession.canSetSessionPreset(.hd1920x1080)
+        }
+        // TODO: Audio input
+        // TODO: Video output (movie)
+        captureSession.commitConfiguration()
+        cameraView.session = captureSession
+    }
 	
+    // Wide Angle Lens (this is on every iphone device through 2019 )
+    // Try for UltraWide and use wide angle for fallback
+    private func bestCamera() -> AVCaptureDevice {
+        if let device = AVCaptureDevice.default(.builtInUltraWideCamera, for: .video, position: .back) {
+            return device
+        }
+        if let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+            return device
+        }
+        
+        fatalError("No camera on the device. Or you are running on the Simulator (not supported")
+    }
+    
 	/// Creates a new file URL in the documents directory
 	private func newRecordingURL() -> URL {
 		let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
--- VideoRecorder/VideoRecorder/ViewController.swift
+++ VideoRecorder/VideoRecorder/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 	
@@ -20,11 +21,40 @@ class ViewController: UIViewController {
 		
 		// TODO: get permission
 		
-		showCamera()
+		requestPermissionAndShowCamera()
 		
 	}
 	
-	private func showCamera() {
+    private func requestPermissionAndShowCamera() {
+        let status = AVCaptureDevice.authorizationStatus(for: .video)
+        
+        switch status {
+            
+        case .notDetermined: // First time user = they haven't seen the dialog to give permission
+            requestPermission()
+        case .restricted: // Parental controls disabled the camera
+            fatalError("Video is diabled for the user (parental controls)")
+            // TODO: Add UI to inform user (talk to parents)
+        case .denied: // User did not give access
+            fatalError("Tell user they need to enable Privacy for Video")
+            // TODO: Add UI to inform user "how to"
+        case .authorized: // Asked for permission (likely last time they used the app)
+            showCamera()
+        @unknown default:
+            fatalError("A new status was added that we need to handle")
+        }
+    }
+    
+    private func requestPermission() {
+        AVCaptureDevice.requestAccess(for: .video) { (granted) in
+            guard granted else {
+                fatalError("Tell user they need to enable Privacy for video")
+            }
+            DispatchQueue.main.async {[weak self] in self?.showCamera()
+            }
+        }
+    }
+    private func showCamera() {
 		performSegue(withIdentifier: "ShowCamera", sender: self)
 	}
 }
